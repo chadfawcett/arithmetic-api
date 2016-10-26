0ef6c1c83be42dbe820307ad3934be3096e28b64
Chad Fawcett <me@chadf.ca>
2016-10-25 23:23:29 -0700

    Set up subtract route

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import addRouter from './router/add'
+import subtractRouter from './router/subtract'
 
 const app = express()
 
 app.use('/add', addRouter)
+app.use('/subtract', subtractRouter)
 
 export default app
