0d7a255ac008ee4a459c6608513f60483ad06e6d
Chad Fawcett <me@chadf.ca>
2016-10-27 17:50:38 -0700

    Add subtract tests

--- src/operation/subtract.unit.js
+++ src/operation/subtract.unit.js
@@ -8,5 +8,7 @@ import subtract from './subtract'
 describe('subtract', () => {
   it('should return the difference of two integers', () => {
     assert.equal(1, subtract(3, 2))
+    assert.equal(3, subtract(6, 3))
+    assert.equal(5, subtract(11, 6))
   })
 })
